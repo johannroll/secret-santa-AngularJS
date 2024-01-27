a48144d4f5631a892f57b9e97783fad2188b64e0
johannroll <johannroll@gmail.com>
2024-01-27 21:23:04 +0200

    update http interceptor 4

--- src/app/app.ts
+++ src/app/app.ts
@@ -647,8 +647,7 @@ app.factory('AuthInterceptor', function($rootScope: any, $q: any, $window: any, 
     },
       responseError: function(response: any) {
           console.log("repsonseError: ", response.data);
-          var token = $window.loacalStorage.getItem('userToken');
-          if (response.status === 401 && token !== null) {
+          if (response.status === 401 && $window.loacalStorage.getItem('userToken') !== null) {
               // Token has expired
               // Handle token expiration, e.g., redirect to login
               var $mdDialog = $injector.get('$mdDialog');
